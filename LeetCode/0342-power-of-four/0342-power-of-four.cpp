43a64f995eb9072098a06b4ac5ebef552ed8ab81
Tviens7 <44408592+Tviens7@users.noreply.github.com>
2025-08-15 17:11:08 -0500

    Time: 0 ms (100%), Space: 7.8 MB (52.57%) - LeetArchive

---
+++ LeetCode/0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n == 1)
+            return true;
+        while (n > 4 && n % 4 == 0)
+            n /= 4;
+        if (n == 4)
+            return true;
+        return false;
+    }
+};
