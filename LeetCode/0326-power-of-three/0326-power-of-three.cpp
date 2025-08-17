eb9e1268b34f9e4f51e70da206a1d4eb43910475
Tviens7 <44408592+Tviens7@users.noreply.github.com>
2025-08-17 10:20:32 -0500

    Time: 1 ms (74.36%), Space: 8.9 MB (77.78%) - LeetArchive

---
+++ LeetCode/0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 1)
+            return true;
+        while (n > 3 && n % 3 == 0)
+            n /= 3;
+        if (n == 3)
+            return true;
+        return false;
+    }
+};
