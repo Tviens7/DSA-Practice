9f292bb376a27020a013096b44092a9cccd819ca
Tviens7 <44408592+Tviens7@users.noreply.github.com>
2025-08-30 12:26:50 -0500

    Time: 4 ms (52.67%), Space: 25.3 MB (35.57%) - LeetArchive

---
+++ LeetCode/0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValidSudoku(std::vector<std::vector<char>>& board) {
+        std::vector<std::unordered_set<char>> rows(9);
+        std::vector<std::unordered_set<char>> cols(9);
+        std::vector<std::unordered_set<char>> boxes(9);
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char num = board[i][j];
+
+                if (num == '.') {
+                    continue;
+                }
+
+                int box_index = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].count(num)) {
+                    return false;
+                }
+
+                if (cols[j].count(num)) {
+                    return false;
+                }
+                
+                if (boxes[box_index].count(num)) {
+                    return false;
+                }
+                
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[box_index].insert(num);
+            }
+        }
+
+        return true;
+    }
+};
